2d08ed6304eff64966286efe1be82a082b8d5577
Denis <demchenkodenis1@gmail.com>
2021-12-19 12:36:30 +0500

    lesson_4

--- authapp/views.py
+++ authapp/views.py
@@ -1,13 +1,11 @@
-from django.contrib import auth, messages
-from django.contrib.auth.decorators import login_required
+from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
 
-from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
-from baskets.models import Basket
+from authapp.forms import UserLoginForm, UserRegisterForm
 
 
 def login(request):
@@ -20,8 +18,8 @@ def login(request):
             if user.is_active:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('index'))
-        # else:
-        #     print(form.errors)
+        else:
+            print(form.errors)
     else:
         form = UserLoginForm()
     context = {
@@ -36,53 +34,17 @@ def register(request):
         form = UserRegisterForm(data=request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Вы успешно зарегестрировались')
             return HttpResponseRedirect(reverse('authapp:login'))
         else:
-            # messages.error(request, form.errors)
             print(form.errors)
     else:
         form = UserRegisterForm()
+
     context = {
         'title': 'Geekshop | Регистрация',
         'form': form,
     }
     return render(request, 'authapp/register.html', context)
-
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.set_level(request, messages.SUCCESS)
-            messages.success(request, 'Вы успешно сохранили профиль')
-        else:
-            messages.set_level(request, messages.ERROR)
-            messages.error(request, 'Профиль не сохранен')
-            # print(form.errors)
-
-    # total_quantity = 0
-    # total_sum = 0
-    # baskets = Basket.objects.filter(user=request.user)
-    # for basket in baskets:
-    #     total_quantity += basket.quantity
-    #     total_sum += basket.sum()
-
-    # baskets = Basket.objects.filter(user=request.user)
-    # total_sum = sum(basket.sum() for basket in baskets)
-    # total_quantity = sum(basket.quantity for basket in baskets)
-
-    context = {
-        'title': 'Geekshop | Профиль',
-        'form': UserProfileForm(instance=request.user),
-        # 'baskets': baskets,
-        # 'total_quantity': total_quantity,
-        # 'total_sum': total_sum,
-        'baskets': Basket.objects.filter(user=request.user)
-    }
-    return render(request, 'authapp/profile.html', context)
-
 
 def logout(request):
     auth.logout(request)
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authapp.User'
