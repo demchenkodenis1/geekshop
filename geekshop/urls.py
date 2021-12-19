5d3d535cb5566c64d51e6ab3f4d9df49e5dac4c9
Denis <demchenkodenis1@gmail.com>
2021-12-19 12:33:35 +0500

    lesson_3

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from mainapp.views import index, products
+from mainapp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products'),
+    path('products/', include('mainapp.urls', namespace='mainapp')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(max_length=128, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    image = models.ImageField(verbose_name='product_image', blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField(max_length=128, blank=True, null=True)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} | {self.category}"
